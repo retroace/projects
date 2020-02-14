15c74d0593cf6ddb67851b9d36dc2b493580091d
Pratik <tuladharpratik9@gmail.com>
2020-02-14 08:35:36 +0545

    pyramid and reverse pyramid code added

---
+++ pyramid.js
@@ -0,0 +1,35 @@
+
+
+
+var type = 2;
+var rows = 5;
+    var a = ''; 
+
+    if (type===1){
+
+        for(var i = 1; i<=rows; i++){
+            for (var j=1; j<=i;j++){
+            a += j + ' ';
+        }
+        console.log(a);
+        a=' ';
+   }
+        
+    }
+
+if (type===2){
+
+    for(var i = rows; i>=0; i--){
+        for (var j=1; j<=i;j++){
+        a += j + ' ';
+    }
+    console.log(a);
+    a=' ';
+}
+    
+}
+
+
+
+
+
